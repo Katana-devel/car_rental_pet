c5d70175cca2585d545c92c455efc34295bf20cf
egorpilipenko <egorpilipenko0@gmail.com>
2025-10-03 20:43:08 +0200

    Currency Exchange wrapper Currency Exchange integration

--- src/api/user/user.py
+++ src/api/user/user.py
@@ -1,12 +1,15 @@
 from fastapi import APIRouter, HTTPException, Depends, Request, status, BackgroundTasks
 from fastapi.security import OAuth2PasswordRequestForm, HTTPAuthorizationCredentials, HTTPBearer
+from redis.asyncio import Redis
 from sqlalchemy import UUID
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.db.database import get_db
+from src.db.redis import get_redis
 from src.repository import user as repositories_users
 from src.schemas.user import UserProfileSchema, UserResponseSchema
 from src.services.auth import auth_service
+from src.services.currency.currency_exchange import check_currency
 from src.services.password_strength import validate_password as password_strength
 
 
@@ -18,17 +21,36 @@ get_refresh_token = HTTPBearer()
 async def add_user_profile_data(
         user_data: UserProfileSchema,
         user_id: UUID = Depends(auth_service.get_current_user),
-        db : AsyncSession = Depends(get_db)
+        db : AsyncSession = Depends(get_db),
+        redis: Redis = Depends(get_redis)
 ):
     user = await repositories_users.get_user_by_id(user_id, db)
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User don't have account")
 
-    if len(user_data.full_name) < 6:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="full name min length is 6 simbols ")
-    user_info = await repositories_users.add_user_profile_data(user_data, user_id, db)
+    if user_data.full_name:
+        if len(user_data.full_name) < 6:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="full name min length is 6 simbols ")
+
+    if user_data.currency:
+        check = await check_currency(user_data.currency, redis)
+        user_data.currency = check
+
+    user_info = await repositories_users.update_user_profile_data(user_data, user_id, db)
+
     if not user_info:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Can't add user details")
 
 
     return user_info
+
+
+@user_router.get("/", response_model=UserResponseSchema, status_code=status.HTTP_201_CREATED)
+async def get_user_data(
+        user_id: UUID = Depends(auth_service.get_current_user),
+        db : AsyncSession = Depends(get_db),
+):
+    user = await repositories_users.get_user_by_id(user_id, db)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    return user
--- src/services/currency/currency_exchange.py
+++ src/services/currency/currency_exchange.py
@@ -1,6 +1,7 @@
 
 from datetime import date
 from decimal import Decimal, ROUND_HALF_UP
+from fastapi import HTTPException, status
 
 from fastapi.params import Depends
 from redis.asyncio import Redis
@@ -26,10 +27,25 @@ async def update_currency_data():
         logger.info("Currency data updated")
 
 
-async def convert(price_usd: float, currency: str, redis: Redis) -> Decimal:
+
+async def convert(price_usd: float, currency: str, redis: Redis):
     rate = await redis.hget("exchange:usd", currency.lower())
-    if rate is None:
-        raise ValueError(f"Currency {currency} not supported")
-    rate = rate.decode()
-    value = Decimal(price_usd) * Decimal(rate)
-    return value.quantize(Decimal("0.00"), rounding=ROUND_HALF_UP)
+    if not rate:
+        currency = "usd"
+        rate = await redis.hget("exchange:usd", currency)
+
+    rate = Decimal(rate.decode())
+    value = Decimal(price_usd) * rate
+
+    price = int(value.quantize(Decimal("1"), rounding=ROUND_HALF_UP))
+
+    return {"currency": currency, "price": price}
+
+
+async def check_currency(currency, redis: Redis):
+    check = await redis.hget("exchange:usd", currency.lower())
+    if not check:
+        currency = "usd"
+        HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="The currency is unknown. The default USD has been set instead.")
+        return currency
+    return currency
