35aafee2ffcb39564c51df503c46809f6cf0c54c
Katana-devel <egorpilipenko0@gmail.com>
2025-10-28 12:30:17 +0100

    Add SSL context to RabbitMQ connection

--- src/services/messages/car_subscription_producer.py
+++ src/services/messages/car_subscription_producer.py
@@ -1,6 +1,7 @@
 import aio_pika
 import json
 from src.core.config.config import rabbitmq_config
+from ssl_context_ingore import ssl_context_ignore
 
 exchange_name = "email_confirmation_exchange"
 queue_name = "email_confirmation_queue"
@@ -11,7 +12,7 @@ exchange = None
 
 async def setup():
     global connection, channel, exchange
-    connection = await aio_pika.connect_robust(rabbitmq_config.AMQP_URL)
+    connection = await aio_pika.connect_robust(rabbitmq_config.AMQP_URL, ssl_context=ssl_context_ignore())
     channel = await connection.channel()
     exchange = await channel.declare_exchange(exchange_name, aio_pika.ExchangeType.DIRECT)
     queue = await channel.declare_queue(name=queue_name, durable=True)
