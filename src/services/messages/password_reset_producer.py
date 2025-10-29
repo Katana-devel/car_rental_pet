ede8da0692e6e7ec6c333625080f0c3fc33e674b
Katana-devel <egorpilipenko0@gmail.com>
2025-10-29 11:33:09 +0100

    Fix import statement for ssl_context_ignore

--- src/services/messages/password_reset_producer.py
+++ src/services/messages/password_reset_producer.py
@@ -4,7 +4,7 @@ import json
 from sqlalchemy import UUID
 
 from src.core.config.config import rabbitmq_config
-from ssl_context_ingore import ssl_context_ignore
+from ssl_context_ignore import ssl_context_ignore
 
 exchange_name = "password_reset_exchange"
 queue_name = "password_reset_queue"
